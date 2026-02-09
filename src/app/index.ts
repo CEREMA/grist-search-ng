d1334515cd59327b79955032b69d7f5427b4472f
Thomas Guillet <thomas@palya.fr>
2026-02-10 00:06:28 +0100

    Implémente la sélection au clic et la mise en évidence de l'élément cliqué

--- src/app/index.ts
+++ src/app/index.ts
@@ -8,12 +8,16 @@ import { TableModule } from "primeng/table";
 interface SearchResult {
   name: string;
   score: number;
+  id: number;
+  index: number;
+  selected: boolean;
 }
 
 interface GristRow {
   titre: string;
   mots_cles: string;
   description: string;
+  id: number;
 }
 
 @Component({
@@ -22,23 +26,29 @@ interface GristRow {
 })
 export class Index {
   protected value = signal("");
-  results: SearchResult[] = [];
+  protected selected = signal(-1);
+  protected results = signal<SearchResult[]>([]);
   fuseSearch?: Fuse<GristRow>;
 
   constructor() {
     effect(() => {
       const v = this.value();
       if (!v || !this.fuseSearch) {
-        this.results = [];
+        this.results.set([]);
         return;
       }
       const results = this.fuseSearch.search(this.value());
-      this.results = results.map((result) => {
-        return {
-          name: result.item.titre,
-          score: Math.round((result.score || 1) * 1000) / 1000,
-        };
-      });
+      this.results.set(
+        results.map((result, i) => {
+          return {
+            name: result.item.titre,
+            score: Math.round((result.score || 1) * 1000) / 1000,
+            id: result.item.id,
+            index: i,
+            selected: false,
+          };
+        }),
+      );
     });
     window.grist?.ready({
       allowSelectBy: true,
@@ -70,5 +80,17 @@ export class Index {
         // includeMatches: true,
       });
     });
+  }
+  click(index: number) {
+    const selected = this.selected();
+    this.results.update((value) => {
+      if (selected !== -1) {
+        value[selected].selected = false;
+      }
+      value[index].selected = true;
+      return value;
+    });
+    this.selected.set(index);
+    window.grist.setCursorPos({ rowId: this.results()[index].id });
   }
 }
