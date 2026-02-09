1b4db58cf3c6ea1a9460bc629e27fbbd834a50eb
Thomas Guillet <thomas@palya.fr>
2026-02-09 22:49:17 +0100

    Met en place fuse.js

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import { Component, effect, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import Fuse from "fuse.js";
 import { ButtonModule } from "primeng/button";
 import { InputTextModule } from "primeng/inputtext";
 import { TableModule } from "primeng/table";
@@ -9,34 +10,65 @@ interface SearchResult {
   score: number;
 }
 
+interface GristRow {
+  titre: string;
+  mots_cles: string;
+  description: string;
+}
+
 @Component({
   templateUrl: "./index.html",
   imports: [FormsModule, ButtonModule, InputTextModule, TableModule],
 })
 export class Index {
   protected value = signal("");
-  results!: SearchResult[];
+  results: SearchResult[] = [];
+  fuseSearch?: Fuse<GristRow>;
 
   constructor() {
     effect(() => {
-      this.results = [
-        {
-          name: this.value(),
-          score: Math.random(),
-        },
-        {
-          name: "ko",
-          score: Math.random(),
-        },
-      ];
+      const v = this.value();
+      if (!v || !this.fuseSearch) {
+        this.results = [];
+        return;
+      }
+      const results = this.fuseSearch.search(this.value());
+      this.results = results.map((result) => {
+        return {
+          name: result.item.titre,
+          score: Math.round((result.score || 1) * 1000) / 1000,
+        };
+      });
     });
-    window.grist.ready({
+    window.grist?.ready({
       allowSelectBy: true,
       requiredAccess: "read table",
     });
 
-    window.grist.onRecords((records: []) => {
-      alert(`record count ${records.length}`);
+    window.grist?.onRecords((records: GristRow[]) => {
+      if (!records) {
+        return;
+      }
+      this.fuseSearch = new Fuse(records, {
+        keys: [
+          {
+            name: "titre",
+            weight: 1,
+          },
+          {
+            name: "mots_cles",
+            weight: 0.5,
+          },
+          {
+            name: "description",
+            weight: 0.1,
+          },
+        ],
+        includeScore: true,
+        ignoreDiacritics: true,
+        ignoreLocation: true,
+        // includeMatches: true,
+      });
     });
   }
 }
